4cb608d7a7e704aaea635bc2aba92ac1e995e018
swathi-ops <swathipottabathini@gmail.com>
2021-08-27 16:36:52 +0530

    examples

--- ui/my-app/src/Variables.js
+++ ui/my-app/src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"http://localhost:49146/api/",
-    PHOTO_URL:"http://localhost:49146/Photos/"
+    API_URL:"http://192.168.0.96:5000/api/",
+    PHOTO_URL:"http://192.168.0.96:5000/Photos/"
 }
